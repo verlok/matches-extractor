8266bb2744365f09d50168431a5f1881d910b6cb
verlok <andrea.verlicchi@gmail.com>
2021-08-12 18:10:12 +0200

    Refactoring step1

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-let openAllCollapsed = () => {
-  let collapsedElements = document.querySelectorAll(
-    ".suf-CompetitionMarketGroup-collapsed"
-  );
-  for (let element of collapsedElements) {
-    element.click();
-  }
-};
+((productionMode = false) => {
+  let openAllCollapsed = () => {
+    let collapsedElements = document.querySelectorAll(
+      ".suf-CompetitionMarketGroup-collapsed"
+    );
+    for (let element of collapsedElements) {
+      element.click();
+    }
+  };
 
-let crunchData = () => {
-  let openChampionshipRows = document.querySelectorAll(
-    ".suf-CompetitionMarketGroup_Open"
-  );
-  let data = [];
-  for (championshipRow of openChampionshipRows) {
-    let championshipContainer = championshipRow.parentElement;
-
-    // Extract team names, all in a single array, they are to regroup A vs B
+  // Extract team names, all in a single array
+  let getTeamNames = (championshipContainer) => {
     let teamNameEls = championshipContainer.querySelectorAll(
       ".rcl-ParticipantFixtureDetailsTeam_TeamName"
     );
-    let teamNamesArr = [];
+    let outArr = [];
     for (let teamNameEl of teamNameEls) {
-      teamNamesArr.push(teamNameEl.innerText);
+      outArr.push(teamNameEl.innerText);
     }
+    return outArr;
+  };
 
-    // Extract odds
+  // Extract odds, all in a single array
+  let getOddsArr = (championshipContainer) => {
     let oddsEls = championshipContainer.querySelectorAll(
       ".sgl-ParticipantOddsOnly80_Odds"
     );
@@ -32,68 +29,85 @@ let crunchData = () => {
     for (let oddsEl of oddsEls) {
       oddsArr.push(oddsEl.innerText);
     }
+    return oddsArr;
+  };
 
-    // Separator line? (not for now)
-    /* let championshipName = championshipContainer.querySelector(
-      ".suf-CompetitionMarketGroupButton_Text"
-    ).innerText;
-    matchesAndOdds.push([`Championship: ${championshipName}`]); */
+  let crunchData = () => {
+    let openChampionshipRows = document.querySelectorAll(
+      ".suf-CompetitionMarketGroup_Open"
+    );
+    let data = [];
+    for (championshipRow of openChampionshipRows) {
+      let championshipContainer = championshipRow.parentElement;
 
-    // Get matches and odds together
-    let matchesCount = teamNamesArr.length / 2;
-    for (let matchIndex = 0; matchIndex < matchesCount; matchIndex += 1) {
-      let teamA = teamNamesArr[matchIndex * 2];
-      let teamB = teamNamesArr[matchIndex * 2 + 1];
-      let odds1 = oddsArr[matchIndex];
-      let oddsX = oddsArr[matchIndex + matchesCount];
-      let odds2 = oddsArr[matchIndex + matchesCount * 2];
-      data.push([teamA, teamB, odds1, oddsX, odds2]);
+      let teamNamesArr = getTeamNames(championshipContainer);
+      let oddsArr = getOddsArr(championshipContainer);
+
+      debugger;
+
+      // Get matches and odds together
+      let matchesCount = teamNamesArr.length / 2;
+      for (let matchIndex = 0; matchIndex < matchesCount; matchIndex += 1) {
+        let teamA = teamNamesArr[matchIndex * 2];
+        let teamB = teamNamesArr[matchIndex * 2 + 1];
+        let odds1 = oddsArr[matchIndex];
+        let oddsX = oddsArr[matchIndex + matchesCount];
+        let odds2 = oddsArr[matchIndex + matchesCount * 2];
+        data.push([teamA, teamB, odds1, oddsX, odds2]);
+      }
     }
-  }
-  return data;
-};
+    return data;
+  };
 
-let makeCsv = (data) => {
-  let csvContent = "data:text/csv;charset=utf-8,";
+  let makeCsv = (data) => {
+    let csvContent = "data:text/csv;charset=utf-8,";
 
-  data.forEach(function (rowArray) {
-    let row = rowArray.join(",");
-    csvContent += row + "\r\n";
-  });
+    data.forEach(function (rowArray) {
+      let row = rowArray.join(",");
+      csvContent += row + "\r\n";
+    });
 
-  let encodedUri = encodeURI(csvContent);
-  let link = document.createElement("a");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "my_data.csv");
-  document.body.appendChild(link);
-  link.click();
-};
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link);
+    link.click();
+  };
 
-let waitUntil = (conditionFn, callback) => {
-  let interval = setInterval(() => {
-    if (conditionFn()) {
-      clearInterval(interval);
-      callback();
-    }
-  }, 500);
-};
+  let waitUntil = (conditionFn, callback) => {
+    let interval = setInterval(() => {
+      if (conditionFn()) {
+        clearInterval(interval);
+        callback();
+      }
+    }, 500);
+  };
 
-let allIsOpen = () =>
-  !document.querySelectorAll(".suf-CompetitionMarketGroup-collapsed").length;
+  let allIsOpen = () =>
+    !document.querySelectorAll(".suf-CompetitionMarketGroup-collapsed").length;
 
-// Let's run this
+  
 
-(() => {
-  let DEVMODE = true;
-  if (DEVMODE) {
+  /*
+   *
+   * NOW LET'S
+   * RUN THIS!
+   * 
+   */
+
+  // Conservative mode, for development
+  if (!productionMode) {
     let data = crunchData();
     console.log(data);
     return;
   }
 
+  // No conservative mode, will expand and download the file
   waitUntil(allIsOpen, () => {
     let data = crunchData();
     makeCsv(data);
   });
   openAllCollapsed();
-})();
+
+})(false); // 👁 remember to change false to true to make it work effectively 👁
